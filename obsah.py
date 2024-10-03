78ee4ca9ab234e7f38d54aa5d1e3bfc42c55c724
RS <ramsh@seznam.cz>
2024-10-03 10:20:13 +0200

    Rekurze

--- obsah.py
+++ obsah.py
@@ -1,8 +1,11 @@
 def obsah_obdelniku():
     a = int(input("Zadej velikost strany a v cm: "))
     b = int(input("Zadej velikost strany b v cm: "))
-    obsah = a * b
-    print(f"Obsah obdélníku o stranách {a} a {b} je {obsah}")
+    if a > 0 and b > 0:
+        obsah = a * b
+        print(f"Obsah obdélníku o stranách {a} a {b} je {obsah}")
+    else:
+        print("Špatné hodnoty. Zkuste znovu.")
+        obsah_obdelniku()
 
 obsah_obdelniku()
-
