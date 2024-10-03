769138b072c568492b888acc17e0cf38647b9127
RS <ramsh@seznam.cz>
2024-10-03 10:12:59 +0200

    Funkce na výpočet obsahu obdélníku

---
+++ obsah.py
@@ -0,0 +1,8 @@
+def obsah_obdelniku():
+    a = int(input("Zadej velikost strany a v cm: "))
+    b = int(input("Zadej velikost strany b v cm: "))
+    obsah = a * b
+    print(f"Obsah obdélníku o stranách {a} a {b} je {obsah}")
+
+obsah_obdelniku()
+
